dd7e1cf9c005c775e89d71240f6d0ff29b325969
Nacholopp <114901316+Nacholopp@users.noreply.github.com>
2025-10-29 23:18:02 -0500

    Update wifi_server.py

--- frontend_tutorial2/wifi_server.py
+++ frontend_tutorial2/wifi_server.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST = "192.168.3.49" # IP address of your Raspberry PI
+HOST = "10.131.111.204" # IP address of your Raspberry PI
 PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -18,4 +18,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     except: 
         print("Closing socket")
         client.close()
+
         s.close()    
